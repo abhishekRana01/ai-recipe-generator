b5b67074863b4d323ad82460bd560e36ee849954
Abhishek Rana <abhishekrana1993@yahoo.in>
2025-12-28 12:28:27 +0530

    Commenting auth for now

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,9 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { data } from "./data/resource";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { auth } from "./auth/resource";
+// import { auth } from "./auth/resource";
 
 const backend = defineBackend({
-  auth,
   data,
 });
 
